c13ebf4ac08882225959b8e6d9c611030069094e
Bibek1998 <bibekbhusansahoo@gmail.com>
2025-05-07 01:36:52 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
 # traditionally to define class variable we use __init__ method
 # but in dataclass we can define class variable directly
 @dataclass
@@ -50,5 +53,8 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 # The above code is a Python script that defines a data ingestion component for a machine learning pipeline.
