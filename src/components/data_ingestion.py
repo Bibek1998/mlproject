86052d7765ffb61e5f668bcc8c34da89e7f27064
Bibek1998 <bibekbhusansahoo@gmail.com>
2025-05-10 02:39:33 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 # traditionally to define class variable we use __init__ method
 # but in dataclass we can define class variable directly
@@ -56,5 +57,9 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
 # The above code is a Python script that defines a data ingestion component for a machine learning pipeline.
