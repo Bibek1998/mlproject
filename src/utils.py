59f82c39183d5ee77062bce61547aee2e8233987
Bibek1998 <bibekbhusansahoo@gmail.com>
2025-05-12 16:46:58 +0530

    Prediction Pipeline and Flask API creation

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+
 import dill
 
 def save_object(file_path, obj):
@@ -46,5 +47,13 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, params):
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
